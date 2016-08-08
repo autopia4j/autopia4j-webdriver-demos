5ead618ee4c8c126b902112bf6c6ce8cdc69a2c2
Vijay Ramaswamy <vijay.ramaswamy@icims.com>
2016-08-08 17:34:24 -0400

    Updated dependency to webdriver fw 1.1.0
    * Updated POM
    * Updated properties files based on recent changes to f/w
    * Updated test.run.configs file based on recent changes to f/w

--- mercurytours.modular/src/test/java/com/autopia4j/demo/mercurytours/modular/testscripts/TestConfigurations.java
+++ mercurytours.modular/src/test/java/com/autopia4j/demo/mercurytours/modular/testscripts/TestConfigurations.java
@@ -13,10 +13,10 @@ public class TestConfigurations {
 		return new Object[][] {
 			{ "Instance1", ExecutionMode.LOCAL, "N/A", Browser.CHROME, Platform.WINDOWS },
 			{ "Instance2", ExecutionMode.LOCAL, "N/A", Browser.FIREFOX_MARIONETTE, Platform.WINDOWS },
-			{ "Instance3", ExecutionMode.PERFECTO_REMOTEWEBDRIVER, "Apple_iPhone_6", Browser.PERFECTO_MOBILE_OS, Platform.ANY },
-			{ "Instance4", ExecutionMode.PERFECTO_REMOTEWEBDRIVER, "Apple_iPad_4", Browser.PERFECTO_MOBILE_SAFARI, Platform.ANY },
-			{ "Instance5", ExecutionMode.PERFECTO_REMOTEWEBDRIVER, "Samsung_Galaxy_S5", Browser.PERFECTO_MOBILE, Platform.ANDROID },
-			{ "Instance6", ExecutionMode.PERFECTO_REMOTEWEBDRIVER, "Samsung_Galaxy_Note_10_1", Browser.PERFECTO_MOBILE_CHROME, Platform.ANDROID }
+			{ "Instance3", ExecutionMode.PERFECTO_DEVICE, "Apple_iPhone_6", Browser.PERFECTO_MOBILE_OS, Platform.ANY },
+			{ "Instance4", ExecutionMode.PERFECTO_DEVICE, "Apple_iPad_4", Browser.PERFECTO_MOBILE_SAFARI, Platform.ANY },
+			{ "Instance5", ExecutionMode.PERFECTO_DEVICE, "Samsung_Galaxy_S5", Browser.PERFECTO_MOBILE, Platform.ANDROID },
+			{ "Instance6", ExecutionMode.PERFECTO_DEVICE, "Samsung_Galaxy_Note_10_1", Browser.PERFECTO_MOBILE_CHROME, Platform.ANDROID }
 		};
 	}
 	
@@ -31,10 +31,10 @@ public class TestConfigurations {
 	@DataProvider(name = "MobileBrowsers", parallel = true)
 	public static Object[][] mobileBrowsers() {
 		return new Object[][] {
-			{ "Instance1", ExecutionMode.PERFECTO_REMOTEWEBDRIVER, "Apple_iPhone_6", Browser.PERFECTO_MOBILE_OS, Platform.ANY },
-			{ "Instance2", ExecutionMode.PERFECTO_REMOTEWEBDRIVER, "Apple_iPad_4", Browser.PERFECTO_MOBILE_SAFARI, Platform.ANY },
-			{ "Instance3", ExecutionMode.PERFECTO_REMOTEWEBDRIVER, "Samsung_Galaxy_S5", Browser.PERFECTO_MOBILE, Platform.ANDROID },
-			{ "Instance4", ExecutionMode.PERFECTO_REMOTEWEBDRIVER, "Samsung_Galaxy_Note_10_1", Browser.PERFECTO_MOBILE_CHROME, Platform.ANDROID }
+			{ "Instance1", ExecutionMode.PERFECTO_DEVICE, "Apple_iPhone_6", Browser.PERFECTO_MOBILE_OS, Platform.ANY },
+			{ "Instance2", ExecutionMode.PERFECTO_DEVICE, "Apple_iPad_4", Browser.PERFECTO_MOBILE_SAFARI, Platform.ANY },
+			{ "Instance3", ExecutionMode.PERFECTO_DEVICE, "Samsung_Galaxy_S5", Browser.PERFECTO_MOBILE, Platform.ANDROID },
+			{ "Instance4", ExecutionMode.PERFECTO_DEVICE, "Samsung_Galaxy_Note_10_1", Browser.PERFECTO_MOBILE_CHROME, Platform.ANDROID }
 		};
 	}
 }
